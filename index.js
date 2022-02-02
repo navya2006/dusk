7c02b457b035ceceaa0abdac82db17a525a0e255
Naahvya <46841818+navya2006@users.noreply.github.com>
2022-02-02 11:47:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// FULLPAGE
+var myFullpage = new fullpage('#fullpage', {
+    navigation: true,
+    scrollOverflow: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Home', 'About Spaceffic', 'The Problem', 'Our Solution', 'Teaser',
+     'Our Team'],
+    css3: false
+});
